6cdd571335d364aaafbaa80564f559c91dd27e24
Brad Bergstrum <bbergstrum@hotmail.com>
2020-02-19 20:40:11 +1100

    implemented basic landing page with CSS.

---
+++ eastside.js
@@ -0,0 +1,29 @@
+
+
+// Display Variables ?
+var showLanding = true;
+
+var showRepacement = false;
+var showBalance = false
+var showAlignment = false;
+var showRepair = false;
+var showAfterHours = false;
+var showCallout = false;
+
+function ChangeDisplay (){
+    //user clicks a card or navbar
+    //change landing info
+    //change service cards based on user input
+    //show card information
+}
+
+//function for rego lookup form submission
+
+//function for tyre check?
+
+//sign up functionality ??
+
+//database implementation
+
+
+
